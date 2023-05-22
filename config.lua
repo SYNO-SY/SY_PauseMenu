975a520be18ff3112c49127e117f01ef8e7da977
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2023-05-22 21:19:24 +0400

    little bit config value changes

--- config.lua
+++ config.lua
@@ -2,8 +2,8 @@ Config                  = {}
 Config.Locale = GetConvar('esx:locale', 'en')
 Config.BotToken = ''  -- How to create a bot token https://www.youtube.com/watch?v=-m-Z7Wav-fM
 Config.EnableDiscordImages = true -- if false will display default images
-Config.MaleDefaultImage = 'https://i.imgur.com/FnYSDHq.png'
-Config.FemaleDefaultImage = 'https://i.imgur.com/1DMGtLP.png'
+Config.DefaultMaleImage = 'https://imgur.com/UqJmo5d.png'
+Config.DefaultFemaleImage = 'https://imgur.com/Tovcqdp.png'
 Config.WebHook = '' --Report system webhook
 Config.webhookName = 'SY_Pausemenu' -- Name of the WebHook
 Config.webhookLogo = 'https://cdn.discordapp.com/attachments/954263572874137671/984808162857222164/discord.png' -- Avatar of the WebHook
